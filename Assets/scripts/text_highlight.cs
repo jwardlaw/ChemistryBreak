5c4704439702c74cb9a77e47378af74397ce116c
binhphan421 <binhphan421@gmail.com>
2015-05-12 13:15:05 -0700

    Started TextHighlight
    started TextHighlight, works for the front 3 chemicals but I haven't
    added it to the others yet.

---
+++ Assets/scripts/text_highlight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class text_highlight : MonoBehaviour {
+	public GameObject ui_text;
+	Text highlight;
+
+	void Start()
+	{
+		highlight = ui_text.GetComponent<Text> ();
+	}
+
+	void OnMouseOver()
+	{
+		highlight.text = this.tag;
+	}
+}
