4a9d766ce574534e4d349529e73a50530e09042e
binhphan421 <binhphan421@gmail.com>
2015-05-19 15:23:40 -0700

    Drag added
    made a script to drag the buttons. still figuring out how to make
    something happen from the drag and drop.

---
+++ Assets/scripts/DragHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public static GameObject item;
+	public Button button1;
+	Vector3 start_position;
+
+	void Start()
+	{
+		button1.GetComponentInChildren<Text> ().text = "pH Strips";
+	}
+
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		item = gameObject;
+		start_position = transform.position;
+	}
+	public void OnDrag (PointerEventData eventData)
+	{
+		transform.position = Input.mousePosition;
+	}
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		item = null;
+		transform.position = start_position;
+	}
+
+}
