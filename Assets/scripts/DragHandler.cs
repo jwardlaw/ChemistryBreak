a2791c5efa8a3c07df6220f564dc122386430abc
binhphan421 <binhphan421@gmail.com>
2015-05-20 09:52:14 -0700

    Finished Toolbar

--- Assets/scripts/DragHandler.cs
+++ Assets/scripts/DragHandler.cs
@@ -5,12 +5,16 @@ using System.Collections;
 
 public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 	public static GameObject item;
+	//public static GameObject item2;
 	public Button button1;
 	Vector3 start_position;
-
+	public GameObject ui_text;
+	Text highlight;
+	
 	void Start()
 	{
-		button1.GetComponentInChildren<Text> ().text = "pH Strips";
+		highlight = ui_text.GetComponent<Text> ();
+		button1.GetComponentInChildren<Text> ().text = gameObject.name;
 	}
 
 
@@ -26,8 +30,57 @@ public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
 	public void OnEndDrag (PointerEventData eventData)
 	{
+		Debug.Log ("called");
+		RaycastHit hit = new RaycastHit();
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if(Physics.Raycast(ray, out hit, 1000)) {
+			if(hit.collider.tag == "Water")
+			{
+				StartCoroutine("DisplayText");
+			}
+			else if(hit.collider.tag == "H2S04")
+			{
+				StartCoroutine("DisplayTextAcid");
+			}
+			else if(hit.collider.tag == "Pheno")
+			{
+				StartCoroutine("DisplayTextBase");
+			}
+			else
+			{
+				StartCoroutine("DisplayTextEtc");
+			}
+		}
 		item = null;
 		transform.position = start_position;
+	}
+
+	IEnumerator DisplayText()
+	{
+		highlight.text = "Nothing happened";
+		yield return new WaitForSeconds (5);
+		highlight.text = "";
+	}
+
+	IEnumerator DisplayTextAcid()
+	{
+		highlight.text = "The pH strip turned red.";
+		yield return new WaitForSeconds (5);
+		highlight.text = "";
+	}
+
+	IEnumerator DisplayTextBase()
+	{
+		highlight.text = "The pH strip turned blue.";
+		yield return new WaitForSeconds (5);
+		highlight.text = "";
+	}
+
+	IEnumerator DisplayTextEtc()
+	{
+		highlight.text = "These cannot be combined";
+		yield return new WaitForSeconds (5);
+		highlight.text = "";
 	}
 
 }
