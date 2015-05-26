c51c37afea10b8a077853041368f8db0ea84bc40
binhphan421 <binhphan421@gmail.com>
2015-05-26 12:59:21 -0700

    shit
    asfasdf

---
+++ Assets/scripts/DoorLock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorLock : MonoBehaviour {
+	public GameObject field;
+	// Use this for initialization
+	void Start () {
+		field.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		field.SetActive (true);
+	}
+}
