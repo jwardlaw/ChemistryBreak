c3c8bb96a45af06afd1366eea3aadf8d383a34f9
yueminn <itsniuyuemin@gmail.com>
2015-05-27 09:19:26 -0700

    Added menu stuff

---
+++ Assets/scripts/StartGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame: MonoBehaviour {
+	void Update()
+	{
+		//start the game if the start game key is pressed
+		if (Input.GetKeyDown ("space"))
+		{
+			Application.LoadLevel("main");
+		}
+	}
+}
